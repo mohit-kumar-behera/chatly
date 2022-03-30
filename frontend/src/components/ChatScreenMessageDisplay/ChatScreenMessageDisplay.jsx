8768d9ae58b93fbe68fc401b8fa4e45dfe6a24dc
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2022-03-30 12:50:07 +0530

    add react-scroll-bottom

--- frontend/src/components/ChatScreenMessageDisplay/ChatScreenMessageDisplay.jsx
+++ frontend/src/components/ChatScreenMessageDisplay/ChatScreenMessageDisplay.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 import './ChatScreenMessageDisplay.css';
 
 const ChatScreenMessageDisplay = () => {
   return (
-    <div className="chat-screen__message-display">
-      <div className="message-div">
-        <p>Hello</p>
+    <ScrollToBottom className="chat-screen__message-display__wrapper">
+      <div className="chat-screen__message-display">
+        <div className="message-div">
+          <p>Hello</p>
+        </div>
+        <div className="message-div myself">
+          <p>
+            How are you?sdugudsy fudsygfdsuygf sdgfo udgfoua dgouags
+            doifugdsoiufg dsiougf iosdug foiuadsg foiudsag foiusdh fiudsh ioug
+          </p>
+        </div>
+        <div className="message-div">
+          <p>I am good and what about you?</p>
+        </div>
+        <div className="message-div">
+          <p>Hii</p>
+        </div>
+        <div className="message-div myself">
+          <p>Hello</p>
+        </div>
+        <div className="message-div myself">
+          <p>
+            How are you?sdugudsy fudsygfdsuygf sdgfo udgfoua dgouags
+            doifugdsoiufg dsiougf iosdug foiuadsg foiudsag foiusdh fiudsh ioug
+          </p>
+        </div>
+        <div className="message-div">
+          <p>I am good and what about you?</p>
+        </div>
       </div>
-      <div className="message-div myself">
-        <p>
-          How are you?sdugudsy fudsygfdsuygf sdgfo udgfoua dgouags doifugdsoiufg
-          dsiougf iosdug foiuadsg foiudsag foiusdh fiudsh ioug
-        </p>
-      </div>
-      <div className="message-div">
-        <p>I am good and what about you?</p>
-      </div>
-      <div className="message-div">
-        <p>Hii</p>
-      </div>
-      <div className="message-div myself">
-        <p>Hello</p>
-      </div>
-    </div>
+    </ScrollToBottom>
   );
 };
 
